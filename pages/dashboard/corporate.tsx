86a458af89b6a88daaa9844018a5dd4f8e4e2f0a
prespan <99643315+prespan@users.noreply.github.com>
2025-04-30 11:05:12 +0300

    Create corporate.tsx

---
+++ pages/dashboard/corporate.tsx
@@ -0,0 +1,9 @@
+import CorporateDashboard from '@/components/dashboards/CorporateDashboard'
+
+export default function Corporate() {
+  return (
+    <main className="p-6">
+      <CorporateDashboard />
+    </main>
+  )
+}
