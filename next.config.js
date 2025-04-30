7b2cc49c79a5bdb0e19501d6eda427a9c8aa5b6a
prespan <99643315+prespan@users.noreply.github.com>
2025-04-30 10:54:03 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
-module.exports = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['storage.googleapis.com', 'up.railway.app'],
+  },
+}
+
+module.exports = nextConfig
