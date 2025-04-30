53ac551f15f8e05d7835a38a83945f28af381007
prespan <99643315+prespan@users.noreply.github.com>
2025-04-30 14:02:11 +0300

    Create investor.tsx

---
+++ pages/dashboard/investor.tsx
@@ -0,0 +1,9 @@
+import InvestorDashboard from '@/components/dashboards/InvestorDashboard'
+
+export default function Investor() {
+  return (
+    <main className="p-6">
+      <InvestorDashboard />
+    </main>
+  )
+}
