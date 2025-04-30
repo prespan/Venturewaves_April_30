0a18c1d8d5ac37974f6740e2b2c32391f6e2212c
prespan <99643315+prespan@users.noreply.github.com>
2025-04-30 11:04:22 +0300

    Create studio.tsx

---
+++ pages/dashboard/studio.tsx
@@ -0,0 +1,9 @@
+import StudioDashboard from '@/components/dashboards/StudioDashboard'
+
+export default function Studio() {
+  return (
+    <main className="p-6">
+      <StudioDashboard />
+    </main>
+  )
+}
