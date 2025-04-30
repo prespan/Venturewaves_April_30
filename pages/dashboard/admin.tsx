e286cd8aab8cd88398e3c4756d3bbba6f76ac7f0
prespan <99643315+prespan@users.noreply.github.com>
2025-04-30 11:06:29 +0300

    Create admin.tsx

---
+++ pages/dashboard/admin.tsx
@@ -0,0 +1,9 @@
+import AdminDashboard from '@/components/dashboards/AdminDashboard'
+
+export default function Admin() {
+  return (
+    <main className="p-6">
+      <AdminDashboard />
+    </main>
+  )
+}
