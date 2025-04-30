206433ae5d8d9f7fb87d545b1f0c43ece4896897
prespan <99643315+prespan@users.noreply.github.com>
2025-04-30 11:07:57 +0300

    Create research.tsx

---
+++ pages/dashboard/research.tsx
@@ -0,0 +1,9 @@
+import ResearchDashboard from '@/components/dashboards/ResearchDashboard'
+
+export default function Research() {
+  return (
+    <main className="p-6">
+      <ResearchDashboard />
+    </main>
+  )
+}
