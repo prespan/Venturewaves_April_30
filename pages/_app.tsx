a41872457272e5eed8c5b0f8733377c0b1c50051
prespan <99643315+prespan@users.noreply.github.com>
2025-05-04 09:07:30 +0000

    Update _app.tsx with ThemeProvider and Layout integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,11 @@ import Layout from '@/components/ui/Layout';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system" // Switches between light/dark based on OS setting
+      enableSystem={true}   // Ensures system theme preference is respected
+    >
       <Layout>
         <Component {...pageProps} />
       </Layout>
