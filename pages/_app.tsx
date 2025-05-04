7c618f9579f9437b09fe0d7109407b7491a9610f
prespan <99643315+prespan@users.noreply.github.com>
2025-05-04 08:57:03 +0000

    Update _app.tsx to include ThemeProvider and Layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
+import Layout from '@/components/ui/Layout';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  );
 }
