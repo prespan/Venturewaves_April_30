566419dc81ad03de966371d3a666aac0bfdacd26
prespan <99643315+prespan@users.noreply.github.com>
2025-05-06 07:25:50 +0000

    Fix next.config.js for standard next start

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
   images: {
     domains: ['storage.googleapis.com', 'up.railway.app'],
   },
