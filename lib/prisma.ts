e587d1f85a43303535feaaec182ed646d2b85628
prespan <99643315+prespan@users.noreply.github.com>
2025-05-08 17:57:53 +0000

    Fix messaging API and Prisma client import

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,4 @@
 // lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-  // Avoid duplicate instances in development
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
-
-export const prisma =
-  global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+export const prisma = new PrismaClient();
