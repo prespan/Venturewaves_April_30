cbf522d82aa11075ce4f3d3fa4f9e869437d8456
prespan <99643315+prespan@users.noreply.github.com>
2025-05-19 07:08:00 +0000

    fix: update tsconfig and next.config for alias and JSX runtime

--- next.config.cjs
+++ next.config.cjs
@@ -1,8 +1,14 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['storage.googleapis.com', 'up.railway.app'],
+  },
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
   },
 };
 
