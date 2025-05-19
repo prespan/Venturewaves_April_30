ad77841b2c1c5941b9c6264baa8977f766ac67f3
prespan <99643315+prespan@users.noreply.github.com>
2025-05-19 19:12:30 +0000

    Add partners tab logic with keyword-matching

--- src/hooks/useCorporatePartners.ts
+++ src/hooks/useCorporatePartners.ts
@@ -1,8 +1,13 @@
-import useSWR from 'swr';
+import useSWR from "swr";
 
-const fetcher = (url: string) => fetch(url).then(res => res.json());
+const fetcher = (url: string) =>
+  fetch(url)
+    .then((res) => {
+      if (!res.ok) throw new Error("Failed to fetch");
+      return res.json();
+    });
 
-export default function useCorporatePartners(corporateId: number) {
+export function useCorporatePartners(corporateId: number) {
   return useSWR(
     corporateId ? `/api/corporates/${corporateId}/partners` : null,
     fetcher
